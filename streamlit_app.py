1992162c8d8bf5b5c82882ec6a35f2998529be91
Matteo Monchiero <matteo@streamlit.io>
2020-07-21 15:10:02 -0700

    credentials

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,21 +2,16 @@ import streamlit as st
 from google.cloud import storage
 import json
 import hashlib
+import os
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "credentials.json"
 
 storage_client = storage.Client()
 
 bucket_name = "recipes-storage"
 
 
-bucket = storage_client.get_bucket(bucket_name, client_options={"credentials_file": "credentials.json"})
-
-user = st.sidebar.text_input("username")
-password = st.sidebar.text_input("password", type="password")
-m = hashlib.sha256()
-m.update(password)
-password_digest = m.digest()
-
-# get file and decrypt
+bucket = storage_client.get_bucket(bucket_name)
 
 mode = st.sidebar.selectbox("Choose one", ["Read recipes", "Add a new recipe"])
 
@@ -25,16 +20,38 @@ if mode == "Read recipes":
         data = json.loads(blob.download_as_string())
         st.markdown(f"# {data['name']}\n{data['description']}\n---")
 else:
-    name = st.text_input("Name")
-    labels = st.text_input("Labels (comma separated)").split(",")
-    description = st.text_area("Description", height=20)
 
-    submit = st.button("Submit")
-    if submit:
-        blob = bucket.blob(name)
-        data = json.dumps({
-            "name": name,
-            "labels": labels,
-            "description": description,
-        })
-        blob.upload_from_string(data, content_type='application/json')
+    user = st.sidebar.text_input("username")
+    password = st.sidebar.text_input("password", type="password")
+    m = hashlib.sha256()
+    m.update(password.encode('utf-8'))
+    entered_password_digest = str(m.hexdigest())
+
+    def get_password_digest(username):
+        with open("people.json", "r") as f:
+            people = json.load(f)
+        for e in people:
+            if e["username"] == username:
+                return e["password"]
+        return None
+
+    password_digest = get_password_digest(user)
+    #st.write(password_digest)
+    #st.write(entered_password_digest)
+
+    if password_digest != entered_password_digest:
+        st.error("Wrong credentials!")
+    else:
+        name = st.text_input("Name")
+        labels = st.text_input("Labels (comma separated)").split(",")
+        description = st.text_area("Description", height=20)
+
+        submit = st.button("Submit")
+        if submit:
+            blob = bucket.blob(name)
+            data = json.dumps({
+                "name": name,
+                "labels": labels,
+                "description": description,
+            })
+            blob.upload_from_string(data, content_type='application/json')
