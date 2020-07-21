37dde0708a1a489209e24e200eb23f92e1dd3b47
Matteo Monchiero <matteo@streamlit.io>
2020-07-21 13:56:29 -0700

    app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,32 @@
 import streamlit as st
+from google.cloud import storage
+import json
 
-st.write("Hello")
+storage_client = storage.Client()
+
+bucket_name = "recipes-storage"
 
 
+bucket = storage_client.get_bucket(bucket_name)
+
+
+mode = st.sidebar.selectbox("Choose one", ["Read recipes", "Add a new recipe"])
+
+if mode == "Read recipes":
+    for blob in bucket.list_blobs():
+        data = json.loads(blob.download_as_string())
+        st.markdown(f"# {data['name']}\n{data['description']}\n---")
+else:
+    name = st.text_input("Name")
+    labels = st.text_input("Labels (comma separated)").split(",")
+    description = st.text_area("Description", height=20)
+
+    submit = st.button("Submit")
+    if submit:
+        blob = bucket.blob(name)
+        data = json.dumps({
+            "name": name,
+            "labels": labels,
+            "description": description,
+        })
+        blob.upload_from_string(data, content_type='application/json')
